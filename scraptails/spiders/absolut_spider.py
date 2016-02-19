4690f37c4de2d685e1a623e5dcd07342c43be4ab
Carlos Edo <carlos.edo@ticketea.com>
2016-02-19 20:24:52 +0100

    fixed spider

--- scraptails/spiders/absolut_spider.py
+++ scraptails/spiders/absolut_spider.py
@@ -6,6 +6,7 @@ class AbsolutSpider(scrapy.Spider):
     name = "absolut"
     allowed_domains = ["absolutdrinks.com"]
     start_urls = ["http://www.absolutdrinks.com/en/drinks/"]
+    # start_urls = ["http://www.absolutdrinks.com/en/drinks/anejo-highball/"]
 
     def parse(self, response):
         for page in range(1):
@@ -22,24 +23,24 @@ class AbsolutSpider(scrapy.Spider):
     def parse_dir_contents(self, response):
         item = CocktailItem()
 
-        item['title'] = response.xpath('//h1[@class="heading"]/text()').extract()[0]
-        item['mixing_instructions'] = response.xpath('//div[@itemprop="recipeInstructions"]/p/text()').extract()[0]
-        item['description'] = response.xpath('//div[@class="text-content"]/p/text()').extract()[0]
+        item['title'] = response.xpath('//h1[@class="heading"]/text()').extract_first()
+        item['mixing_instructions'] = response.xpath('//div[@itemprop="recipeInstructions"]/p/text()').extract_first(default='')
+        item['description'] = response.xpath('//div[@class="text-content"]/p/text()').extract_first(default='')
 
         ingredients = response.xpath('//ul[@class="ingredient"]/li')
         item['ingredients'] = []
 
         for ingredient in ingredients:
-            measure = ingredient.xpath('//li/text()').split(' ')
+            measure = ingredient.xpath('text()').extract_first().split(' ')
             item['ingredients'].append({
-                'ingredient': ingredient.xpath('//li/a'),
+                'ingredient': ingredient.xpath('a/text()').extract_first(),
                 'amount': measure[0],
                 'measurement': measure[1]
             })
 
         extra_contents = response.xpath('//div[@class="hide-mobile"]/h4')
-        item['glass_type'] = extra_contents[0].xpath('span/a/text()').extract()[0]
+        item['glass_type'] = extra_contents[0].xpath('span/a/text()').extract_first(default='')
         item['scraped_tastes'] = extra_contents[1].xpath('span/a/text()').extract()
-        item['skill_level'] = extra_contents[2].xpath('span/a/text()').extract()[0]
+        item['skill_level'] = extra_contents[2].xpath('span/a/text()').extract_first(default='')
 
         yield item
